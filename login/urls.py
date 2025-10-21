0c5b1d01c621c87e9aa11a6e67817113b8667906
Misha <misatimosyn540@gmail.com>
2025-10-21 09:50:03 +0300

    Up

--- login/urls.py
+++ login/urls.py
@@ -3,7 +3,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import Registration
 
 urlpatterns = [
-    path('register/', Registration.as_view()),
+    path('registr/', Registration.as_view()),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
